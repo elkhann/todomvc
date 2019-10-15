5ded9bb6109487574b121becc224d9e4a0de9193
Emil Khann <emilkhann@gmail.com>
2019-10-15 15:06:32 +0300

    add redux devtools

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ import rootReducer from './reducers';
 import 'antd/dist/antd.css';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer /* preloadedState, */,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 render(
   <Provider store={store}>
--- src/reducers/searchTodo.js
+++ src/reducers/searchTodo.js
@@ -1,7 +1,6 @@
 const searchTodo = (state = '', action) => {
   switch (action.type) {
     case 'SEARCH_FILTER':
-      console.log('search filter', action.search);
       return action.search;
 
     default:
